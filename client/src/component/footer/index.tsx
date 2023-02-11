6cb92ba30960f0498ce528def39605b02de24424
Ayomide <ayomidelawal700@gmail.com>
2023-02-11 18:20:54 +0100

    footer collapse now possible

--- client/src/component/footer/index.tsx
+++ client/src/component/footer/index.tsx
@@ -47,7 +47,7 @@ const FooterCollapse = ({ setCollapse }: FooterCollapseProps) => (
             <li>Support & resources</li>
           </ul>
         </div>
-        <span onClick={() => setCollapse(false)}>
+        <span onClick={() => setCollapse(false)} className="expand__icon">
           <svg
             viewBox="0 0 32 32"
             xmlns="http://www.w3.org/2000/svg"
