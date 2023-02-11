671900474f35ed8fdf97d32846dfd1f2efd6ea8a
Ayomide <ayomidelawal700@gmail.com>
2023-02-11 18:20:09 +0100

    toggling menu list

--- client/src/component/header/index.tsx
+++ client/src/component/header/index.tsx
@@ -1,22 +1,27 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import airbnbLogo from '../../assets/logo.svg';
 import './style.css';
 import { Menu } from '../../ui/icon/menu';
 import { World } from '../../ui/icon/world';
 import { Person } from '../../ui/icon/person';
-import { useLocation } from 'react-router-dom';
+import { MenuList } from '../../ui/menuList';
+import { useState } from 'react';
 
 const Header = () => {
+  const [openMenuList, setOpenMenuList] = useState(false);
   const pathname = useLocation().pathname;
+  const navigate = useNavigate();
+  const shouldOmitSearch = /^[/]?(?:login|signup)/.test(pathname);
   return (
     <header className="header">
       <div className="header-wrapper section-wrapper">
-        <div className="logo-wrapper">
+        <div className="logo-wrapper" onClick={() => navigate('/')}>
           <span>
             <img src={airbnbLogo} alt="website logo" />
             <p>airbnb</p>
           </span>
         </div>
-        {!/^[/]?login/.test(pathname) ? (
+        {!shouldOmitSearch ? (
           <div className="search-wrapper">
             <span>Anywhere</span>
             <span>Any week</span>
@@ -50,11 +55,22 @@ const Header = () => {
             <World />
           </span>
           <span className="group__2">
-            <Menu />
+            <span
+              onClick={() => setOpenMenuList((status) => !status)}
+              style={{ cursor: 'pointer' }}
+            >
+              <Menu />
+            </span>
             <span className="person-icon">
               <Person />
             </span>
           </span>
+          <div
+            className="menu-list-wrapper"
+            style={{ display: openMenuList ? 'initial' : 'none' }}
+          >
+            <MenuList close={() => setOpenMenuList(false)} />
+          </div>
         </div>
       </div>
     </header>
@@ -62,7 +78,3 @@ const Header = () => {
 };
 
 export { Header };
-
-//eye <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-// <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
-// </svg>
