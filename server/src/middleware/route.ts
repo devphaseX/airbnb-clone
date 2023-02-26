37c2a976092a088482ed0918cea0e90e78cd8afc
Ayomide <ayomidelawal700@gmail.com>
2023-02-26 19:10:13 +0100

    update the place create route

--- server/src/middleware/route.ts
+++ server/src/middleware/route.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
 import { userRouter } from '../router/user';
 import { imageRouter } from '../router/image';
+import { placeRouter } from '../router/place';
 
 const mountRoute = (app: Express) => {
   app.use(imageRouter);
   app.use('/auth', userRouter);
+  app.use('/place', placeRouter);
 };
 
 export { mountRoute };
