df509b078b121fa0c0a6dfb4c465012d13f1a23c
Ayomide <ayomidelawal700@gmail.com>
2023-02-26 19:15:34 +0100

    handle image upload

---
+++ client/src/store/api/uploadApi.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosRequestConfig } from 'axios';
+import { fetchFn } from './baseUrl';
+
+const uploadImage = (
+  data: FormData,
+  signal: AbortSignal,
+  onUploadProgress?: AxiosRequestConfig['onUploadProgress']
+) => {
+  return fetchFn((baseUrl) =>
+    axios.post(`${baseUrl}/upload`, data, {
+      method: 'POST',
+      withCredentials: true,
+      headers: { 'Content-Type': 'multipart/form' },
+      onUploadProgress,
+      signal,
+    })
+  )();
+};
+
+export { uploadImage };
