12c930de0af5782a9a3ef6b42cf9726a237262ef
Ayomide <ayomidelawal700@gmail.com>
2023-03-02 15:11:15 +0100

    add other support route

--- server/src/middleware/route.ts
+++ server/src/middleware/route.ts
@@ -2,11 +2,12 @@ import { Express } from 'express';
 import { userRouter } from '../router/user';
 import { imageRouter } from '../router/image';
 import { placeRouter } from '../router/place';
+import { protectedAuthRoute } from '../controller/auth';
 
 const mountRoute = (app: Express) => {
   app.use(imageRouter);
   app.use('/auth', userRouter);
-  app.use('/place', placeRouter);
+  app.use('/place', protectedAuthRoute(), placeRouter);
 };
 
 export { mountRoute };
